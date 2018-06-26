3eb8b3238450410b9f9ae118d1b8b20a0fc2c654
Adriana Capriles <adrianacapriles@gmail.com>
2018-06-25 20:11:01 -0400

    now its working

--- game.js
+++ game.js
@@ -1,54 +1,52 @@
 var heroes = ["Captain America", "Hulk", "Iron Man", "Thor"];
 
 function displayHeroes() {
-    var hero = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
+   var hero = $(this).attr("data-name");
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
-    $.ajax({
-        url:queryURL,
-        method:'GET'
-    })
-    .done(function(response){
-        console.log(response);
-        $("#heroesView").empty();
-        
-        results = response.data;
-        for (var i=0; i<results.legth; i++) {
-            var rating = results[i].rating;
-            var heroImage = results[i].images.fixed_height.url;
-            var heroStill = results[i].images.fixed_height_still.url;
+   $.ajax({url:queryURL, method:'GET'}).done(function(response){
+       console.log(response);
+       $("#heroesView").empty();
+   
+       for (var i=0; i<response.data.length; i++) {
+           results = response.data[i];
+           var rating = results.rating;
+           var heroImage = results.images.fixed_height.url;
+           var heroStill = results.images.fixed_height_still.url;
 
-            var image = $("<img>");
-            var p = $("<p id='rating'>" + "Rating: " + rating + "</p>");
+           var image = $("<img>");
+           var p = $("<p id='rating'>" + "Rating: " + rating + "</p>");
 
-            image.attr('src', heroStill);
-            image.attr('alt', 'hero');
-            image.attr('data-state', 'still');
-            image.attr('data-still', heroStill);
-            image.attr('data-animate', heroImage);
+           image.attr('src', heroStill);
+           image.attr('alt', 'hero');
+           image.attr('data-state', 'still');
+           image.attr('data-still', heroStill);
+           image.attr('data-animate', heroImage);
 
-            $("#heroesView").prepend(image,p);
-            checkState();
-            
-        }
-    });
+           $("#heroesView").prepend(image,p);
+           checkState();
+           console.log();
+       }
+   });
 }
 function renderButtons() {
-    $("#buttons-view").empty();
-    for (var i = 0; i < heroes.length; i++) {
-        var newButton =$("<button>");
-        newButton.addClass("newHero");
-        newButton.attr("data-name", heroes[i]);
-        newButton.text(heroes[i]);
-        $("#buttons-view").append(newButton);
-    }
+   $("#buttons-view").empty();
+   for (var i = 0; i < heroes.length; i++) {
+       var newButton =$("<button>");
+       newButton.addClass("newHero");
+       newButton.attr("data-name", heroes[i]);
+       newButton.text(heroes[i]);
+       $("#buttons-view").append(newButton);
+   }
 }
 
 $("#add-hero").on("click", function() {
-    event.preventDefault();
-    var newHero = $("#hero-input").val().trim();
-    heroes.push(newHero);
-    renderButtons();
+   event.preventDefault();
+   var newHero = $("#hero-input").val().trim();
+   heroes.push(newHero);
+   renderButtons();
+    return false;
+  // console.log();
 
 })
 
@@ -56,15 +54,15 @@ $(document).on("click", ".newHero", displayHeroes);
 renderButtons();
 
 function checkState() {
-    image.on('click', function(){
-        var state = $(this).attr("data-state");
-        if (state == "still") {
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', $(this).data('animate'));
-        } 
-        else {
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
+   $("img").on('click', function(){
+       var state = $(this).attr("data-state");
+       if (state == "still") {
+           $(this).attr('src', $(this).data('animate'));
+           $(this).attr('data-state', 'animate');
+       }
+       else {
+           $(this).attr('src', $(this).data('still'));
+           $(this).attr('data-state', 'still');
+       }
+   });
 };
